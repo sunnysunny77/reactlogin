817e9279e98644b3f247fe4b321028222e1fe483
sunnysunny77 <46119793+sunnysunny77@users.noreply.github.com>
2022-08-08 18:08:04 +0800

    up

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ import {
 import Layout from "./pages/Layout";
 import Home from "./pages/Home";
 import Auth from "./pages/Auth";
+import Error from "./pages/Error";
 import './App.scss';
 import "bootstrap/dist/css/bootstrap.min.css"
-import spinner from './images/spinner.gif';
 
 function App() {
 
@@ -43,11 +43,7 @@ function App() {
 
   if (error) {
     return (
-      <section className="loadingSection">
-        <h1 className="text-secondary">Error</h1>
-        <p className="text-secondary">{error}</p>
-        <img src={spinner} alt="Loading..." />
-      </section>
+      <Error error={error}/>
     )
   } else if (login === true) {
     return (
