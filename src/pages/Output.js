2ec9072cbaa58736c7cbab5238a44dd3b98e5352
sunnysunny77 <46119793+sunnysunny77@users.noreply.github.com>
2022-08-10 17:54:38 +0800

    up

--- src/pages/Output.js
+++ src/pages/Output.js
@@ -1,11 +1,8 @@
-import spinner from '../images/spinner.gif';
-
 const Output = (props) => {
     const {load} = props;
     return (
       <main className="loadingSection">
         <p className="text-secondary">{load}</p>
-        <img src={spinner} alt="Loading..." />
       </main>
     );
 }
