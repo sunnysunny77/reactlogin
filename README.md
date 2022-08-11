f263135d9b5e492b5524f29ebe215deb46acc6bc
sunnysunny77 <46119793+sunnysunny77@users.noreply.github.com>
2022-08-11 10:25:21 +0800

    up

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 .env REACT_APP_KEY needs to match up with api
+
+setupProxy.jy proxy to origin
--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://192.168.64.18',
+      target: 'http://192.168.64.19',
       changeOrigin: true,
     })
   );
