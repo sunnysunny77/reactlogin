c07c71c99d8b6a4b50a3c281bbfdc681edc91fa7
sunnysunny77 <46119793+sunnysunny77@users.noreply.github.com>
2022-08-13 21:12:13 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -43,14 +43,14 @@ function App() {
         return res.json()
       })
       .then(data => {
-      
+
         if (data === btoa(process.env.REACT_APP_KEY)) {
 
           setCookie(true)
           setLoad(false)
           return
         }
-    
+
         setCookie(data)
         setLoad(false)
       })
@@ -61,7 +61,7 @@ function App() {
   }
 
   useEffect(() => {
-    
+
     initial()
   }, [])
 
