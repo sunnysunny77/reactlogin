2bef1c2383d3b3e9e680be36e340a7bfaccaea9b
sunnysunny77 <46119793+sunnysunny77@users.noreply.github.com>
2022-10-12 18:40:15 +0800

    up

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -1,8 +1,16 @@
-import React, { useEffect } from "react";
+import ReactDOM from "react-dom"
+import React, { useState, useEffect } from "react";
 import { ClipboardPulse } from 'react-bootstrap-icons';
 import sunflower from "../images/sunflower.jpg";
 
+let PayPalButton
+
 const Admin = () => {
+
+    if (window.paypal) PayPalButton = window.paypal.Buttons.driver("react", { React, ReactDOM });
+
+    const [count, setCount] = useState(1)
+    const [load, setLoad] = useState(false)
 
     useEffect(() => {
 
@@ -10,69 +18,85 @@ const Admin = () => {
         script.setAttribute('src', process.env.REACT_APP_PAYPAL_ID + "&currency=AUD")
         script.setAttribute('nonce', "xyz123")
         script.setAttribute('data-csp-nonce', "xyz123")
-        document.head.appendChild(script)
-        script.addEventListener("load", () => {
+        script.async = true;
+        script.onload = () => {
 
-            window.paypal.Buttons({
-
-                style: {
-                    layout: 'horizontal'
-                },
-                createOrder: (data, actions, err) => {
-
-                    return actions.order.create({
-
-                        purchase_units: [
-                            {
-                                description: "Sunflower",
-                                amount: {
-                                    currency_code: "AUD",
-                                    value: 20,
-                                },
-                            },
-                        ]
-                    })
-                },
-                onApprove: async (data, actions) => {
-
-                    const order = await actions.order.capture()
-
-                    const description = order.purchase_units[0].description;
-                    const orderID = order.id;
-                    const email = order.payer.email_address;
-                    const name = order.purchase_units[0].shipping.name.full_name
-                    let address = "";
-                    for (let x in order.purchase_units[0].shipping.address) {
-                        address +=
-                            order.purchase_units[0].shipping.address[x] + "<br>";
-                    }
-                    const total = " $" + order.purchase_units[0].amount.value
-                    const output =
-                        "<h2>" +
-                        description +
-                        "</h2><br><br><h3>ID:</h3><br>" +
-                        orderID +
-                        "<br><br><h3>Email:</h3><br>" +
-                        email +
-                        "<br><br><h3>Name:</h3><br>" +
-                        name +
-                        "<br><br><h3>Address:</h3><br>" +
-                        address +
-                        "<br><br><h3>Total:</h3><br>" +
-                        total
-
-                    const approved = document.getElementById("approved")
-                    approved.innerHTML = output
-                    approved.className = "alert alert-primary"
-                },
-                onError: (err) => {
-
-                    document.getElementById("approved").innerHTML = err
-                }
-            }).render("#payPal")
-        })
+            setLoad(true)
+        };
+        document.head.appendChild(script);
     }, [])
 
+    const createOrder = (data, actions) => {
+
+        const value = count * 20
+
+        return actions.order.create({
+
+            purchase_units: [
+                {
+                    description: "Securewebsite Transaction",
+                    amount: {
+                        currency_code: "AUD",
+                        value: value,
+                        breakdown: {
+                            item_total: {
+                                currency_code: "AUD",
+                                value: value
+                            }
+                        }
+                    },
+                    items: [
+                        {
+                            name: "Sunflower",
+                            unit_amount: {
+                                currency_code: "AUD",
+                                value: "20"
+                            },
+                            quantity: count,
+                        },
+                    ]
+                }],
+        });
+    };
+    const onApprove = async (data, actions) => {
+
+        const order = await actions.order.capture()
+
+        const description = order.purchase_units[0].description;
+        const orderID = order.id;
+        const email = order.payer.email_address;
+        const name = order.purchase_units[0].shipping.name.full_name
+        let address = "";
+        for (let x in order.purchase_units[0].shipping.address) {
+            address +=
+                order.purchase_units[0].shipping.address[x] + "<br>";
+        }
+        const purchase =
+            order.purchase_units[0].items[0].quantity +
+            " x " +
+            order.purchase_units[0].items[0].name +
+            " $" +
+            order.purchase_units[0].items[0].unit_amount.value +
+            "<br><br>Total: $" +
+            order.purchase_units[0].amount.value;
+        const output =
+            "<h2>" +
+            description +
+            "</h2><br><br><h3>ID:</h3><br>" +
+            orderID +
+            "<br><br><h3>Email:</h3><br>" +
+            email +
+            "<br><br><h3>Name:</h3><br>" +
+            name +
+            "<br><br><h3>Address:</h3><br>" +
+            address +
+            "<br><h3>Purchase:</h3><br>" +
+            purchase
+
+        const approved = document.getElementById("approved")
+        approved.innerHTML = output
+        approved.className = "alert alert-primary"
+    };
     return (
         <React.Fragment>
             <br />
@@ -82,10 +106,38 @@ const Admin = () => {
             <br />
             <br />
             <img id="sunflower" src={sunflower} alt="sunflower" />
-            <div id="payPal"></div>
+            <div id="payPal">
+                <div className="counter">
+                    <span
+                        onClick={() => {
+
+                            count > 1 ? setCount(count - 1) : setCount(count)
+                        }}
+                        >
+                        -
+                    </span>
+                    <input disabled={true} id="quantity" type="text" value={count} />
+                    <span
+                        onClick={() => {
+
+                            setCount(count + 1)
+                        }}
+                        >
+                        +
+                    </span>
+                </div>
+                {load ? (
+                    <PayPalButton id="payPal"
+                        createOrder={(data, actions) => createOrder(data, actions)}
+                        onApprove={(data, actions) => onApprove(data, actions)}
+                    />
+                ) : (
+                    <React.Fragment/>
+                )
+                }
+            </div>
             <div id="approved"></div>
         </React.Fragment>
     );
 }
-
 export default Admin;
