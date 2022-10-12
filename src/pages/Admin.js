4b92fd02a59ac67d635b4612e57684821bedab9d
sunnysunny77 <46119793+sunnysunny77@users.noreply.github.com>
2022-10-12 21:38:59 +0800

    Revert "Update Admin.js"
    This reverts commit b2a3728beba9b2a8646cf4a424f19c1d34747cb1.

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -20,12 +20,10 @@ const Admin = () => {
         script.onload = () => {
 
             PayPalButton = window.paypal.Buttons.driver("react", { React, ReactDOM });
-            setLoad(
-                <PayPalButton
-                    createOrder={(data, actions) => createOrder(data, actions)}
-                    onApprove={(data, actions) => onApprove(data, actions)}
-                />  
-            )
+            setLoad( <PayPalButton
+                createOrder={(data, actions) => createOrder(data, actions)}
+                onApprove={(data, actions) => onApprove(data, actions)}
+            />)
         };
         document.head.appendChild(script);
     }, [])
