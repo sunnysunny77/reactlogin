0ea4621dbd8a84b8829a5b077dbceb478297fe56
sunnysunny77 <46119793+sunnysunny77@users.noreply.github.com>
2023-02-14 21:49:11 +0800

    up

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,16 +6,26 @@ import stationaryLandscape from "../images/stationary-landscape.webp";
 const Home = () => {
     return (
         <div className="row g-0 p-3 align-items-center align-items-lg-end justify-content-between flex-column-reverse flex-sm-row">
-            <img className="col-12 col-sm-4" src={stationary} alt="stationary"/>
+            <p className="col-12 col-sm-4 mb-0 ">  
+                <img src={stationary} alt="stationary"/> 
+                <span className="d-block p-3"> 
+                    Ut enim ad minim veniam quis nostrud exercitation ullamco laboris. 
+                </span>   
+            </p>
             <section className="col-12 col-sm-7 row g-0 text-end justify-content-end">
                 <h1 >Lorem ipsum dolor</h1>
                 <HouseDoor className="col-1"/> 
-                <p className="mt-3 mb-sm-0 p-3 order-4 order-sm-3">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
-                    nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
-                    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                <p className="mt-3 mb-sm-0 p-3 order-4 order-sm-3 row g-0 align-items-center justify-content-between">
+                    <b className="col-12 col-lg-4 text-start mb-3">
+                        Adipiscing elit
+                    </b>
+                    <span className="col-12 col-lg-7">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+                        nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
+                        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                    </span>
                 </p>  
                 <img className="mt-3 order-3 order-sm-4"  src={stationaryLandscape} alt="stationary-landscape"/>
             </section>
--- src/pages/Navigation.js
+++ src/pages/Navigation.js
@@ -28,6 +28,7 @@ const Navigation = (props) => {
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
               <Nav.Link accessKey='1' as={Link} to="/" > Home </Nav.Link>
+              <Nav.Link accessKey='2' as={Link} to="auth" > Store </Nav.Link>
             </Nav>
             <Nav>
               <Nav.Link accessKey='2' as={Link} to="auth"  > Sign in <ArrowUpCircle /> </Nav.Link>
