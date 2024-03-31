1635fa2c5cd3c51eb7845ab0af29aa356a693add
daniel <contractor@lateralaspect.com.au>
2024-03-31 10:38:23 +0800

    up

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,17 @@
 import React from "react";
 import stationary from "../images/stationary.webp";
-import stationaryLandscape from "../images/stationary-landscape.webp";
 
 const Home = () => {
     return (
-        <div className="row g-0 p-3 align-items-center align-items-lg-end justify-content-between flex-column flex-sm-row">
-            <p className="order-2 order-sm-1 col-10 m-0 col-sm-4">  
+        <div className="row g-0 p-3 align-items-center  justify-content-around justify-content-xl-between align-items-xl-end flex-column flex-sm-row">
+            <p className="col-11 mb-5 mt-3 my-md-0 col-md-4">  
                 <img src={stationary} alt="stationary"/> 
-                <b className="d-block p-3"> 
+                <b className="d-block px-3 pb-4 pt-5"> 
                    Ut enim ad minim veniam quis nostrud exercitation ullamco laboris. 
                 </b>   
             </p>
-            <section className="order-1 order-sm-2 col-12 col-sm-7 row d-flex flex-column flex-sm-row g-0 text-end justify-content-end">
-                <p className="col-11 col-sm-12 mx-auto mb-5 mb-sm-0 p-5 p-lg-3 order-4 order-sm-3 row g-0 align-items-center justify-content-between">
+            <section className="col-12 col-md-7 row d-flex flex-column flex-sm-row g-0 text-end justify-content-end">
+                <p className="col-11 col-md-12 mx-auto mb-0 p-5 p-lg-3 row g-0 align-items-center justify-content-between">
                     <h1 className="col-12 col-lg-4 text-start text-lg-center mb-3">
                       Adipiscing elit
                     </h1>
@@ -23,8 +22,9 @@ const Home = () => {
                         nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
                         reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
                     </span>
-                </p>  
-                <img className="col-11 col-sm-12 mx-auto order-3 order-sm-4"  src={stationaryLandscape} alt="stationary-landscape"/>
+                </p>
+                <div className="col-11 col-md-12 mx-auto position-relative overflow-hidden">
+                </div>
             </section>
         </div>
     );
--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -10,7 +10,7 @@ const Layout = (props) => {
     <>
       <a className="d-none" accessKey="s" href="#main">skip navigation</a>
       <Navigation logOut={logOut} />
-      <main id="main" className="py-sm-5">
+      <main id="main" className="py-sm-4">
         <Outlet />
       </main>
       <footer className="row g-0 justify-content-center align-items-center text-center">
