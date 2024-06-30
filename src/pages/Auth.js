3806fd338a313f54b0ac2dbf84ef3152ad89efbe
daniel <contractor@lateralaspect.com.au>
2024-06-30 10:18:46 +0800

    Update Auth.js

--- src/pages/Auth.js
+++ src/pages/Auth.js
@@ -36,8 +36,7 @@ const Auth = (props) => {
 
     const canvas = document.getElementById("mainCaptcha")
 
-    const alpha = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-      'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+    const alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
       '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
     let txt = ""
@@ -46,7 +45,6 @@ const Auth = (props) => {
 
       txt += alpha[Math.floor(Math.random() * alpha.length)]
     }
-
     const context = canvas.getContext('2d')
     canvas.width = 140
     canvas.height = 50
