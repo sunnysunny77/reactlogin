374fface006ca6910b2af4f80a5d3451e7061e97
daniel <contractor@lateralaspect.com.au>
2024-08-25 13:38:20 +0800

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -23,7 +23,7 @@ const Home = () => {
                         sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
                         Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
                         nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
-                        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                        reprehender.
                     </span>
                 </p>
                 <div className="col-11 col-md-12 mx-auto position-relative overflow-hidden">
