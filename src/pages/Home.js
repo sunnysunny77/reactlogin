e3f7e4f391fec198ec3610d676d4d819a8e0e463
daniel <contractor@lateralaspect.com.au>
2024-09-01 14:22:23 +0800

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,7 @@ import stationary from "../images/stationary.webp";
 const Home = () => {
     return (
         <div className="row g-0 p-3 align-items-center justify-content-around justify-content-xl-between align-items-xl-end flex-column flex-sm-row">
-            <div className="blend col-11 mt-3 mb-5 my-md-0 col-md-4">
+            <div className="blend col-12 col-md-4 mt-3 mb-5 my-md-0">
                 <img  src={stationary} alt="stationary"/> 
                 <p className="m-0">  
                     <b className="d-block px-3 pb-4 pt-5"> 
@@ -14,7 +14,7 @@ const Home = () => {
             </div>
             <section className="col-12 col-md-7 row d-flex flex-column flex-sm-row g-0 text-end justify-content-end">
                 <h1 className="hidden">Home</h1>
-                <p className="col-11 col-md-12 mx-auto mb-0 px-3 py-5 p-md-3 row g-0 align-items-center justify-content-between">
+                <p className="col-12 mx-auto mb-0 px-3 py-5 p-md-3 row g-0 align-items-center justify-content-between">
                     <span className="col-12 col-lg-4 text-start text-lg-center mb-3">
                       Adipiscing elit
                     </span>
@@ -26,7 +26,7 @@ const Home = () => {
                         reprehender.
                     </span>
                 </p>
-                <div className="col-11 col-md-12 mx-auto position-relative overflow-hidden">
+                <div className="col-12 mx-auto position-relative overflow-hidden">
                 </div>
             </section>
         </div>
