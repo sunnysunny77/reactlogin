787fa816b1a9a464dc34be82bd20cc56dee81c63
daniel <contractor@lateralaspect.com.au>
2024-12-05 13:16:47 +0800

    up

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,101 +3,105 @@ import Store from "../images/store.webp";
 import Finance from "../images/finance.webp";
 import Transport from "../images/transport.webp";
 import Warehouse from "../images/warehouse.webp";
+import Vegies from "../images/vegies.webp";
+import Day from "../images/day.webp";
+import Greens from "../images/greens.webp";
 import Carousel from 'react-bootstrap/Carousel';
 import Header from "./Header";
+import Slider from "./Slider";
 import { Link } from "react-router-dom";
 
 const Home = (props) => {
 
   const { logOut } = props;
 
-    return (
-    <>
-      <Header heading="HOME" >
+  return (
+  <>
+    <Header heading="HOME" >
 
-        <div className="col-12 row justify-content-between bg-11 g-0 p-4 p-sm-5">
+      <div className="col-12 row justify-content-between bg-11 g-0 p-4 p-sm-5">
 
-          <p className="row col-12 col-sm-8 col-lg-7 col-xl-5 d-flex align-items-center justify-content-between p-4 g-0">
+        <p className="row col-12 col-sm-8 col-lg-7 col-xl-5 d-flex align-items-center justify-content-between p-4 g-0">
 
-            <span className="col-11 col-md-9">Ut enim ad ed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.</span>
+          <span className="col-11 col-md-9">Ut enim ad ed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.</span>
 
-            {logOut ? (
+          {logOut ? (
 
-              <Link to="store" className="col-auto align-self-md-end border rounded mt-4 mb-3 px-2 py-1" > store </Link>
+            <Link to="store" className="col-auto align-self-md-end border rounded mt-4 mb-3 px-2 py-1" > store </Link>
 
-            ) : (
+          ) : (
 
-              <Link to="auth" className="col-auto align-self-md-end border rounded mt-4 mb-3 px-2 py-1" > store </Link>
+            <Link to="auth" className="col-auto align-self-md-end border rounded mt-4 mb-3 px-2 py-1" > store </Link>
 
-            )}
+          )}
+
+        </p>
+
+        <Carousel pause={false} interval={1000} controls={false} className="col-12 col-sm-4 ps-sm-5">
+
+          <Carousel.Item>
+
+            <img src={Finance} alt="Finance" />
+
+          </Carousel.Item>
+
+          <Carousel.Item>
+
+            <img src={Transport} alt="Transport" />
+
+          </Carousel.Item>
+
+          <Carousel.Item>
+
+            <img src={Warehouse} alt="Warehouse" />
+
+          </Carousel.Item>
+
+        </Carousel>   
+
+      </div>  
+
+    </Header>
+
+    <div className="container py-sm-5 my-5">
+
+      <div className="row g-0 align-items-center justify-content-around flex-column flex-sm-row">
+
+        <div className="blend col-11 col-md-5 mb-5 my-md-0">
+
+          <img  src={Store} alt="store"/> 
+
+          <p className="m-0">  
+
+            <b className="d-block px-3 pb-4 pt-5"> 
+
+              Ut enim ad minim veniam quis nostrud exercitation ullamco laboris. 
+
+            </b>
 
           </p>
 
-          <Carousel pause={false} interval={1000} controls={false} className="col-12 col-sm-4 ps-sm-5">
+        </div>
 
-            <Carousel.Item>
+        <div className="homeLeft col-11 col-md-6 row d-flex flex-column flex-sm-row g-0 text-end justify-content-end">
 
-              <img src={Finance} alt="Finance" />
+          <p className="col-12 mx-auto mb-0 px-3 py-5 p-md-3 row g-0 align-items-center justify-content-between">
 
-            </Carousel.Item>
+            <span className="col-12 col-xl-4 text-start text-xl-center mb-3">
 
-            <Carousel.Item>
+              Adipiscing elit
 
-              <img src={Transport} alt="Transport" />
+            </span>
 
-            </Carousel.Item>
+            <span className="col-12 col-xl-7">
 
-            <Carousel.Item>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+              sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+              Ut enim ad minim veniam, quis nostrud exercitation.
+              
+            </span>
 
-              <img src={Warehouse} alt="Warehouse" />
-
-            </Carousel.Item>
-
-          </Carousel>   
-
-        </div>  
-
-      </Header>
-
-      <div className="container py-sm-5 my-5">
-
-        <div className="row g-0 align-items-center justify-content-around flex-column flex-sm-row">
-
-          <div className="blend col-11 col-md-5 mb-5 my-md-0">
-
-            <img  src={Store} alt="store"/> 
-
-            <p className="m-0">  
-
-              <b className="d-block px-3 pb-4 pt-5"> 
-
-                Ut enim ad minim veniam quis nostrud exercitation ullamco laboris. 
-
-              </b>
-
-            </p>
-
-          </div>
-
-          <div className="homeLeft col-11 col-md-6 row d-flex flex-column flex-sm-row g-0 text-end justify-content-end">
-
-            <p className="col-12 mx-auto mb-0 px-3 py-5 p-md-3 row g-0 align-items-center justify-content-between">
-
-              <span className="col-12 col-xl-4 text-start text-xl-center mb-3">
-
-                Adipiscing elit
-
-              </span>
-
-              <span className="col-12 col-xl-7">
-
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                Ut enim ad minim veniam, quis nostrud exercitation.
-                
-              </span>
-
-            </p>
+          </p>
 
           <div className="col-12 col-md-12 mx-auto position-relative overflow-hidden"></div>
 
@@ -106,8 +110,18 @@ const Home = (props) => {
       </div>
 
     </div> 
-  </>
-  );
+
+    <Slider>
+
+      <img className="has-current counters" src={Day} width="950" height="574" alt="Day" />
+
+      <img className="counters" src={Vegies} width="950" height="574" alt="Vegies" />
+
+      <img className="counters" src={Greens} width="950" height="574" alt="Greens" />
+
+    </Slider>
+
+  </>);
 }
 
 export default Home;
---
+++ src/pages/Slider.js
@@ -0,0 +1,100 @@
+import React, { useState, useRef } from "react";
+import { ArrowRight } from 'react-bootstrap-icons';
+import { ArrowLeft } from 'react-bootstrap-icons';
+
+const Slider= (props) => {
+
+  const [index, setIndex] = useState(0);
+
+  const counters = useRef();
+
+  const first = (index) => {
+
+    counters.current.children[index].classList.add("has-fade");
+
+    setTimeout(()=>{
+
+      counters.current.children[index].classList.remove("has-current");
+      counters.current.children[index].classList.remove("has-fade");
+    },100);
+  }
+
+  const last = (index) => {
+
+    setTimeout(()=>{
+
+      counters.current.children[index].classList.add("has-current");
+      counters.current.children[index].classList.add("has-fade");
+      setTimeout(()=>{
+
+        counters.current.children[index].classList.remove("has-fade");
+      },100);
+    },100);
+  }
+
+  const prev = () => {
+
+    first(index);
+
+    setIndex(index - 1);
+
+    if (index === 0) {
+
+      setIndex(counters.current.children.length - 1);
+
+      last(counters.current.children.length - 1)
+
+      return
+    }
+
+    last(index - 1)
+  };
+
+  const next = () => {
+
+    first(index);
+
+    setIndex(index + 1);
+
+    if (index === counters.current.children.length - 1) {
+
+      setIndex(0);
+
+      last(0)
+
+      return
+    }
+
+    last(index + 1)
+  };
+
+  return (
+	  <div className="container slider_9-container my-5 pb-5 g-0">
+
+      <div className="row justify-content-between align-items-center g-0">
+
+        <div className="col-2 d-flex justify-content-center">
+
+          <button onClick={prev}  aria-label="prev" className="slider_9-button button-prev"><ArrowLeft /></button>
+
+        </div>
+
+        <div ref={counters} className="col-8 slider_9-inner inner-swap position-relative">
+
+          {props.children}
+          
+        </div>
+
+        <div className="col-2 d-flex justify-content-center">
+
+          <button onClick={next} aria-label="next" className="slider_9-button button-next"><ArrowRight /></button>
+
+        </div>
+
+      </div>
+    
+    </div>
+  )
+}
+
+export default Slider;
