2019718e047eb82a14fa72c93edb51ef488745c7
daniel <contractor@lateralaspect.com.au>
2024-12-11 07:03:51 +0800

    up

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,14 +4,14 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://localhost:8888',
+      target: 'http://localhost:8888/api',
       changeOrigin: true,
     })
   );
   app.use(
     '/captcha',
     createProxyMiddleware({
-      target: 'https://localhost:3006',
+      target: 'https://localhost:3006/captcha',
       changeOrigin: true,
       secure: false,
     })
