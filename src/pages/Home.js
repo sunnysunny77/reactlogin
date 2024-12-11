f837b8b9a7d18483a8c3c8233b9407d0407a5a7a
daniel <contractor@lateralaspect.com.au>
2024-12-11 13:36:41 +0800

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -224,11 +224,11 @@ const Home = (props) => {
 
         <div className="curve col-12 col-md-9 col-xxl-7 d-flex justify-content-end px-3 px-md-4 py-sm-5 px-lg-5">
 
-          <div className="inner-curve col-11 col-md-10 col-lg-11 d-flex flex-column g-0 ">  
+          <div className="inner-curve col-11 col-md-10 col-lg-11 col-xxl-12 d-flex flex-column g-0 ">  
 
             <div className="row justify-content-end mb-0 px-3 py-5 p-md-3 g-0">
 
-              <b className="col-12 col-lg-4 text-lg-center mb-3 mb-md-4 mb-lg-0 mt-lg-4">
+              <b className="col-12 col-lg-4 text-lg-center mb-3 mb-md-4 mb-lg-0 mt-lg-2">
 
                 Adipiscing elit
 
